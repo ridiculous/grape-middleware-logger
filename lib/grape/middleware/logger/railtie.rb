db00fe6c04bdf83588bce52035139a139f1f5cfc
Ryan Buckley <arebuckley@gmail.com>
2019-03-07 16:22:06 -0800

    [#24] Change initializer order only for older Rails versions (< 5)

--- lib/grape/middleware/logger/railtie.rb
+++ lib/grape/middleware/logger/railtie.rb
@@ -1,5 +1,6 @@
 class Grape::Middleware::Logger::Railtie < Rails::Railtie
-  initializer 'grape.middleware.logger', after: :load_config_initializers do
+  options = Rails::VERSION::MAJOR < 5 ? { after: :load_config_initializers } : {}
+  initializer 'grape.middleware.logger', options do
     Grape::Middleware::Logger.logger = Rails.application.config.logger || Rails.logger.presence
     Grape::Middleware::Logger.filter = ActionDispatch::Http::ParameterFilter.new Rails.application.config.filter_parameters
   end
